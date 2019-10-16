ad8cb6bb7d56aeefbc0954b16d813805ac51b9cd
Ambroise Roussel <ambroise.roussel.etu@univ-lille.fr>
2019-10-16 23:25:12 +0200

    code clean + reaction rate in config

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,33 @@
 const Discord = require('discord.js');
+const request = require('request');
 const client = new Discord.Client();
+const utils = require('./utils.js');
 const auth = require('../auth.json');
 const reactions = require('../custom-reactions.json');
-const request = require('request');
-const utils = require('./utils.js');
+
 const spongebob = 'media/Mocking-Spongebob.jpg';
+const discordToken = auth.debug ? auth.discordToken : process.env.DISCORD_TOKEN;
+const giphyToken = auth.debug ? auth.giphyToken : process.env.GIPHY_TOKEN;
 
 let init = function () {
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    let i = utils.randomInt(reactions.actions.length);
-    let choice = reactions.actions[i];
-    client.user.setActivity(choice.activity, { type: choice.action });
+    client.user.setActivity(reactions.action.activity, { type: reactions.action.action });
   });
   
   client.on('message', msg => {
     if (msg.author.username + '#' + msg.author.discriminator != client.user.tag) {
-      console.log('Reacting to message from ' + msg.author.username);
-      react(msg);
+      let reacting = utils.randomInt(reactions.reactionChance);
+      if (reacting > 0) {
+        console.log('Reacting to message from ' + msg.author.username);
+        react(msg);
+      }
+      else {
+        console.log('Ignoring message from ' + msg.author.username);
+      }        
     }
   });
-
-  let token = auth.debug ? auth.discordToken : process.env.DISCORD_TOKEN;
-  console.log(token);
-  client.login(token);
+  client.login(discordToken);
 }
 
 let react = function (msg) {
@@ -63,8 +67,8 @@ let sendRandomGIF = function (msg) {
   let searches = reactions.gifSearch;
   let i = utils.randomInt(searches.length);
   let query = searches[i];  
-  let token = auth.debug ? auth.giphyToken : process.env.GIPHY_TOKEN;
-  let url = 'http://api.giphy.com/v1/gifs/search?api_key=' + token + '&q=' + query;
+  
+  let url = 'http://api.giphy.com/v1/gifs/search?api_key=' + giphyToken + '&q=' + query;
   request(url, { json : true }, (err, res, body) => {
     if (res.statusCode === 200) {
       let i = utils.randomInt(body.data.length);
