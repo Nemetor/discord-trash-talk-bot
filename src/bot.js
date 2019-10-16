0b8817010115e2144663079f9f1ae21f6279e171
Ambroise Roussel <ambroise.roussel.etu@univ-lille.fr>
2019-10-17 00:17:45 +0200

    sound on connect

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,8 @@ const auth = require('../auth.json');
 const reactions = require('../custom-reactions.json');
 
 const spongebob = 'media/Mocking-Spongebob.jpg';
+const jeanne = 'media/jeanne.mp3';
+
 const discordToken = auth.debug ? auth.discordToken : process.env.DISCORD_TOKEN;
 const giphyToken = auth.debug ? auth.giphyToken : process.env.GIPHY_TOKEN;
 
@@ -14,7 +16,6 @@ let init = function () {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity(reactions.action.activity, { type: reactions.action.action });
   });
-  
   client.on('message', msg => {
     if (msg.author.username + '#' + msg.author.discriminator != client.user.tag) {
       let reacting = utils.randomInt(reactions.reactionChance);
@@ -24,10 +25,18 @@ let init = function () {
       }
       else {
         console.log('Ignoring message from ' + msg.author.username);
-      }        
+      }
     }
   });
-  client.login(discordToken);
+  client.on("voiceStateUpdate", function (oldMember, newMember) {
+    let channel = newMember.voiceChannel;
+    let guild = newMember.guild.id;
+    if (channel && newMember.user.tag !== client.user.tag && !client.guilds.get(guild).voiceConnection) {
+      console.log(`Detecting member join`);
+      speak(channel);
+    }
+  });
+  client.login(discordToken).catch(console.error);
 }
 
 let react = function (msg) {
@@ -57,8 +66,8 @@ let repeatMockingly = function (msg) {
   let res = '';
   let big = true;
   for (let letter of msg.content) {
-      res += big ? letter.toUpperCase() : letter.toLowerCase();
-      big = !big;
+    res += big ? letter.toUpperCase() : letter.toLowerCase();
+    big = !big;
   }
   msg.channel.send(res, new Discord.Attachment(spongebob));
 }
@@ -66,15 +75,26 @@ let repeatMockingly = function (msg) {
 let sendRandomGIF = function (msg) {
   let searches = reactions.gifSearch;
   let i = utils.randomInt(searches.length);
-  let query = searches[i];  
-  
+  let query = searches[i];
   let url = 'http://api.giphy.com/v1/gifs/search?api_key=' + giphyToken + '&q=' + query;
-  request(url, { json : true }, (err, res, body) => {
+  request(url, { json: true }, (err, res, body) => {
     if (res.statusCode === 200) {
       let i = utils.randomInt(body.data.length);
       msg.channel.send(body.data[i].url);
     }
+    else {
+      console.log(err);
+    }
   });
+}
+
+let speak = function (channel) {
+  channel.join().then((cnx) => {
+    let dispatcher = cnx.playFile(jeanne);
+    dispatcher.on('end', (end) => {
+      channel.leave();
+    });        
+  }).catch(console.error);
 }
 
 init();
